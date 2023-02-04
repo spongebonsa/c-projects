1b25d2dd893b9fb420813a4443b75fcbb62c9a2c
bonsa Getachew <66962080+spongebonsa@users.noreply.github.com>
2023-02-04 18:17:10 +0300

    simple code excutes typed int

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    cout << "enter a number:";
+    cin >> num1;
+    cout << "you entered:" << num1 << endl;
+    
+    return 0;
+}
